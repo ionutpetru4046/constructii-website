d099a7aacdfd55220645a32943adbb7945947065
ionutpetru4046 <ionut4046@yahoo.com>
2026-02-25 19:04:57 +0000

    working on footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FiPhone, FiMail, FiMapPin, FiClock, FiFacebook, FiInstagram } from "rea
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-t from-gray-950 via-gray-900 to-gray-800 text-gray-300">
+    <footer className="bg-linear-to-t from-gray-950 via-gray-900 to-gray-800 text-gray-300">
       <div className="max-w-7xl mx-auto px-6 py-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
 
